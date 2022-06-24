9a9d4dab17906ba9a640b8d3a78cf39d1185817b
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-06-24 15:51:06 +0530

    while_loop done

---
+++ while_loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>while_loop in php</title>
+</head>
+<body>
+    <?php
+        $x = 1;
+
+        while($x<=5){
+            echo "The Number is: ".$x."<br>";
+            $x++;
+        }
+    ?>
+</body>
+</html>
