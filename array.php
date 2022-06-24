83435eb8fad06982bdbebd3fd42631dfaa5f2948
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-06-24 16:27:34 +0530

    array done

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array in php</title>
+</head>
+<body>
+    <?php
+        $arr = array("hero", "bmw", "benz");
+        echo "I like".$arr[0].", ".$arr[1]. " &".$arr[2]."<br>";
+        echo count($arr) ;
+    ?>
+</body>
+</html>
